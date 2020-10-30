c914796f27a6f8707843a6f49fddce04dcacda44
Philipp Kessling <philipp.kessling@haw-hamburg.de>
2020-10-30 16:55:59 +0100

    add advanced tests for recursively defined classes

--- test/renderer/utils/ValueRegistry.spec.ts
+++ test/renderer/utils/ValueRegistry.spec.ts
@@ -1,30 +1,85 @@
 import { assert } from "chai";
-import { ValueRegistry } from "../../../src/renderer/utils/registry"
+import { Test } from "mocha";
+import { Registry, ValueRegistry } from "../../../src/renderer/utils/registry"
 
 describe('ValueRegistry', () => {
 
-    const reg = new ValueRegistry<number>()
+    describe("Basic Suite", () => {
+        const reg = new ValueRegistry<number>()
 
-    describe(".register", () => {
-        it("should accept a new value", () => {
+        describe(".register", () => {
+            it("should accept a new value", () => {
+                assert.isOk(
+                    reg.registerValue({
+                        id: "Bert",
+                        value: 42
+                }));
+            });
+            it("should reject a duplicated value", () => {
+                assert.isNotOk(
+                    reg.registerValue({
+                        id: "Bert",
+                        value: 42
+                    })
+                );
+            });
+            
+        });
+        describe(".getRegisteredValue", () => {
             reg.registerValue({
-                id: "Bert",
-                value: 42
+                id: "val1",
+                value: 1
             });
 
-            assert(reg.registry.length === 1);
-        });
-        it("should reject a duplicated value", () => {
-            assert.throws(() => {
-                reg.registerValue({
-                    id: "Bert",
-                    value: 42
-                });
+            it("should return undefined of a unknown value is requested", () => {
+                assert(reg.getRegisteredValue("val2") === undefined);
             });
         });
-        it("should deregister a value absed on its id")
+        describe(".deregisterValue", () => {
+            it("should deregister a value absed on its id", () => {
+                reg.deregisterValue("bert");
+                assert(reg.getRegisteredValue("bert") === undefined);
+            });
+        });
     });
-    describe(".getRegisteredValue", () => {
-        it("should return undefined of a unknown value is requested");
+
+    describe("Advanced Types Test Suite", () => {
+        
+        class TestClass {
+            id: string
+            childrenId?: string | TestClass
+
+            constructor(id: string, childrenId?: string) {
+                this.id = id;
+                if (childrenId) this.childrenId = childrenId
+            }
+
+            denormalize(registry: ValueRegistry<TestClass>) {
+                if (this.childrenId && !(this.childrenId instanceof TestClass)) {
+                    const data = registry.getRegisteredValue(this.childrenId);
+                    if (data) {
+                        this.childrenId = data;
+                        return true
+                    }
+                }
+                return false
+            }
+        }
+
+        const t1 = new TestClass("T1");
+        const t2 = new TestClass("T2", "T1")
+
+        const reg = new ValueRegistry<TestClass>();
+        reg.registerValue({id: "T1", value: t1});
+        reg.registerValue({id: "T2", value: t2});
+        
+        describe(".getRegisteredValue", () => {
+            it("should do something", () => {
+                assert.isOk(t2.denormalize(reg));
+            });
+            it("should do something2", () => {
+                assert.deepEqual(t2.childrenId, t1);
+            });
+        });
     });
 })

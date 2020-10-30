961e301bdf92dd7162ca411cbbe99b1975ad53c5
Philipp Kessling <philipp.kessling@haw-hamburg.de>
2020-10-30 16:53:34 +0100

    add JSDoc

--- src/renderer/utils/registry.ts
+++ src/renderer/utils/registry.ts
@@ -1,14 +1,12 @@
+
 // class Singleton<T> {
 //     private static instance: T
-
 //     public static getInstance(): T {
 //         if(!Singleton.instance) {
 //             Singleton.instance = new this();
 //         }
-
 //         return Singleton.instance
 //     }
-
 // }
 
 
@@ -17,20 +15,22 @@ interface Class<T> {
     new(...args: unknown[]): T
 }
 
-interface IObjectRegistryEntries<T> {
+export interface IRegistryEntry<T> {
+    // Index signature to limit possible keys as string and property values to either string, template type or undefined.
+    // [key: string]: string | T | Class<T> | undefined
     name: string
     class: Class<T>
 }
 
 /**
- * Creates a registry object
+ * Creates a registry object of a superclass which inherits to classes to be stored in this registry
  * 
- * @template {T} T  Superclass which inherits to classes to be stored in this registry
+ * @template T
  */
 export class Registry<T> {
     // extends Singleton<Registry<T>>
 
-    private registry: IObjectRegistryEntries<T>[]
+    public registry: IRegistryEntry<T>[]
 
     public constructor () {
         // super();
@@ -38,17 +38,27 @@ export class Registry<T> {
     }
 
     /**
+     * registers a subclass of T to the registry
      * 
-     * @param entries {IObjectRegistryEntries<T>[]}     entries to register
+     * @template T
+     * @param {IRegistryEntry<T>[]} entries entries to register
      */
-    public register(entries: IObjectRegistryEntries<T>[]): void {
+    public register(entries: IRegistryEntry<T>[]): void {
         this.registry.push(...entries);
     }
+}
 
     /**
+ * registers class constructors inheriting from a common superclass and exposes their contructors
+ */
+export class ClassRegistry<T> extends Registry<T> {
+
+    /**
+     * returns a new instance of the requested registered class
      * 
-     * @param of {string}   name of the class to get a new instance of
-     * @returns {T}
+     * @template T
+     * @param {string} of name of the class to get a new instance of
+     * @returns {T} returns a new instance of type T
      */
     public getNewInstance(of: string): T {
         return new (this.registry.filter(it => it.name === of)[0].class)();

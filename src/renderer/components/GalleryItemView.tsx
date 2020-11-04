076a049076b74ff5fb9ea354fc2186f2b065b9e7
Philipp Kessling <philipp.kessling@haw-hamburg.de>
2020-11-04 22:13:20 +0100

    rewrite to use composition with Draggable

--- src/renderer/components/GalleryItemView.tsx
+++ src/renderer/components/GalleryItemView.tsx
@@ -1,5 +1,6 @@
 import { IReactionDisposer, reaction } from 'mobx';
 import { Component, h } from 'preact';
+import { Draggable } from './Draggable';
 import { IItem } from './IItem';
 
 interface IGalleryItemViewProps<T extends IItem> {
@@ -25,24 +26,9 @@ export class GalleryItemView extends Component<IGalleryItemViewProps<IItem>> {
     }
     
     render({ item, children, onClick }: IGalleryItemViewProps<IItem>): h.JSX.Element {
-        // let activeClass = "list-group-item";
-
-        return <li
-            class="list-group-item gallery-item"
-            onClick={() => {
-                if (onClick) onClick();
-                // activeClass = activeClass + " active";
-                // TODO: add hover effect!
-            }}
-            // onDblClick={onDblClick}
-            draggable={true}
-            onDragStart={e => {
-                if (e.target) {
-                    e.dataTransfer?.setData("text", item.id);
-                }
-            }}>
-            {children}
-        </li>;
+        return <Draggable id={item.id}>
+            <li class="gallery-item list-group-item" onClick={onClick}>{children}</li>
+        </Draggable>
     }
 
     componentWillUnmount(): void {

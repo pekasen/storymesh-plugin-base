c923a31374cdeaed74da25095930e6e306b43f4a
Philipp Kessling <philipp.kessling@haw-hamburg.de>
2020-11-04 21:47:14 +0100

    base Moveable on IItem

--- src/renderer/components/Moveable.tsx
+++ src/renderer/components/Moveable.tsx
@@ -1,17 +1,31 @@
 import { Component, h } from "preact";
 import { reaction, IReactionDisposer } from "mobx";
 
-import { MoveableItem } from '../store/MoveableItem';
+import { IItem } from './IItem';
 
-interface IMoveableProps {
-    item: MoveableItem
+interface IDataItem extends IItem {
+    name: string
+}
+
+interface IMoveableItem<T extends IDataItem> {
+    data: T
+    x: number
+    y: number
+    updatePosition(x: number, y: number): void
+}
+
+interface IMoveableProps<T extends IDataItem> {
+    item: IMoveableItem<T>
     children: preact.JSX.Element
 }
 
-export class Moveable extends Component<IMoveableProps> {
+/**
+ * Creates a moveable div and bind its data to a mobx store
+ */
+export class Moveable<T extends IDataItem> extends Component<IMoveableProps<T>> {
     reactionDisposer: IReactionDisposer
 
-    constructor(props: IMoveableProps) {
+    constructor(props: IMoveableProps<T>) {
         super(props);
 
         this.reactionDisposer = reaction(
@@ -22,46 +36,48 @@ export class Moveable extends Component<IMoveableProps> {
         )
     }
 
-    render ({ item, children}: IMoveableProps): h.JSX.Element { 
-            return <div
-                style={
-                    "position: absolute;" +
-                    (item.x === 0 ? "" : `left: ${item.x};`) +
-                    (item.y === 0 ? "" : `top: ${item.y};`)
-                }
-                onMouseDown={ 
-                    (e: h.JSX.TargetedMouseEvent<HTMLDivElement>) => {
-                        e = e || window.event;
-                        e.preventDefault();
+    render ({ item, children}: IMoveableProps<T>): h.JSX.Element { 
+        return <div
+            style={
+                "position: absolute;" +
+                (item.x === 0 ? "" : `left: ${item.x};`) +
+                (item.y === 0 ? "" : `top: ${item.y};`)
+            }
+            onMouseDown={ 
+                (e: h.JSX.TargetedMouseEvent<HTMLDivElement>) => {
+                    e = e || window.event;
+                    e.preventDefault();
 
-                        const x = e.clientX;
-                        const y = e.clientY;
-                        const x_0 = item.x;
-                        const y_0 = item.y;
+                    const x = e.clientX;
+                    const y = e.clientY;
+                    const x_0 = item.x;
+                    const y_0 = item.y;
 
-                        function updater (this: Document, event: MouseEvent) {
-                            const _event = event as h.JSX.TargetedMouseEvent<HTMLDocument>
-                            // _event.prevententDefault();
+                    function updater (this: Document, event: MouseEvent) {
+                        const _event = event as h.JSX.TargetedMouseEvent<HTMLDocument>
+                        // _event.prevententDefault();
 
-                            const _x = _event.clientX;
-                            const _y = _event.clientY;
-                            const d_x = (_x - x);
-                            const d_y = (_y - y);
-                            item.updatePosition(
-                                d_x + x_0,
-                                d_y + y_0
-                            );
-                        }
-
-                        function remover () {
-                            document.removeEventListener("mousemove", updater)
-                            document.removeEventListener("mouseup", remover);
-                        }
-                        
-                        document.addEventListener("mousemove", updater);
-                        document.addEventListener("mouseup", remover);
+                        const _x = _event.clientX;
+                        const _y = _event.clientY;
+                        const d_x = (_x - x);
+                        const d_y = (_y - y);
+                        item.updatePosition(
+                            d_x + x_0,
+                            d_y + y_0
+                        );
                     }
-                }>{children}</div>
+
+                    function remover () {
+                        document.removeEventListener("mousemove", updater)
+                        document.removeEventListener("mouseup", remover);
+                    }
+                    
+                    document.addEventListener("mousemove", updater);
+                    document.addEventListener("mouseup", remover);
+                }
+            }>
+                {children}
+            </div>
     }
 
     componentWillUnmount(): void {

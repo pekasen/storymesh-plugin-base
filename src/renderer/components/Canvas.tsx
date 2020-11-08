621b3ba8dc2a81dc8de09f883d275d21776e45c4
Philipp Kessling <philipp.kessling@haw-hamburg.de>
2020-11-08 09:28:55 +0100

    add Canvas stub

---
+++ src/renderer/components/Canvas.tsx
@@ -0,0 +1,9 @@
+import { FunctionalComponent, h } from 'preact';
+
+export const Canvas: FunctionalComponent = () => (
+    <canvas
+        height={800}
+        width={600}
+    >
+    </canvas>
+)

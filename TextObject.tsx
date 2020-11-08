d63d89609a6cd909bd66b46daa511c07f21fcd36
Philipp Kessling <philipp.kessling@haw-hamburg.de>
2020-11-08 09:29:17 +0100

    mhhh

--- TextObject.tsx
+++ TextObject.tsx
@@ -1,29 +1,92 @@
 import { h } from "preact";
 import StoryGraph from 'storygraph';
+import { IContent } from 'storygraph/dist/StoryGraph/IContent';
+import { IEdge } from 'storygraph/dist/StoryGraph/IEdge';
+import { IGraph } from 'storygraph/dist/StoryGraph/IGraph';
+import { IMetaData } from 'storygraph/dist/StoryGraph/IMetaData';
+import { IReactiveInput } from 'storygraph/dist/StoryGraph/IReactiveInput';
+import { IReactiveOutput } from 'storygraph/dist/StoryGraph/IReactiveOutput';
+import { IRenderingProperties } from 'storygraph/dist/StoryGraph/IRenderingProperties';
+import { IStoryModifier } from 'storygraph/dist/StoryGraph/IStoryModifier';
+import { IStoryObject } from 'storygraph/dist/StoryGraph/IStoryObject';
+import { rootStore } from '../renderer';
 import {IPlugInRegistryEntry, IPlugIn, IMenuTemplate } from "../renderer/utils/PlugInClassRegistry";
-
+import { v4 } from "uuid";
+import { computed, makeObservable, observable } from 'mobx';
+import { ContentType } from 'storygraph/dist/StoryGraph/ContentType';
 /**
  * Our first little dummy PlugIn
  * 
  * @todo It should actually inherit from StoryObject and not StoryGraph...
  */
-class _TextObject extends StoryGraph implements IPlugIn{
-    
+// @observable
+class _TextObject implements IPlugIn, IStoryObject{
+    id = v4();
+    userDefinedProperties: any;
+    content?: IContent | undefined;
+    metaData: IMetaData;
+    outgoing: IEdge[];
+    incoming: IEdge[];
+    parent?: StoryGraph | undefined;
+    network?: IGraph | undefined;
+    renderingProperties: IRenderingProperties;
+    modifiers: IStoryModifier[];
+    outputs?: IReactiveOutput | undefined;
+    inputs?: IReactiveInput[] | undefined;
+    isContentNode = true;
+
     constructor() {
-        super();
+        this.renderingProperties = {
+            width: 100,
+            order: 1,
+            collapsable: false
+        };
+        this.modifiers = [];
+        this.outgoing = [];
+        this.incoming = [];
+        this.metaData = {
+            createdAt: new Date(Date.now()),
+            name: "NGWebS user",
+            tags: []
+        };
+        this.content = {
+            resource: "empty",
+            altText: "empty",
+            type: ContentType.text
+        };
+        this.userDefinedProperties = {};
+
+        makeObservable(this, {
+            id: false,
+            userDefinedProperties: observable,
+            content:    observable,
+            metaData:   observable,
+            outgoing:   observable,
+            incoming:   observable,
+            modifiers:  observable,
+            anotherGetter: computed
+            // menuTemplate: computed
+            // inputs:     observable,
+            // outputs:    observable,
+            // parent:     observable,
+            // network:    observable
+        })
     }
 
-    public getMenuTemplate(): IMenuTemplate[]  {
-
-       
+    // @computed
+    get menuTemplate(): IMenuTemplate[]  {
         return [
             {
                 label: "Text",
                 type: "text",
-                valueReference: this.edges,
+                valueReference: null,
                 valueTemplate: ""
             }
         ]
+    }
+
+    get anotherGetter(): string {
+        return "Hello"
     }
 
     public render(): h.JSX.Element {
@@ -31,10 +94,27 @@ class _TextObject extends StoryGraph implements IPlugIn{
     }
 }
 
-export const TextObject: IPlugInRegistryEntry<StoryGraph> = {
+/**
+ * Define the metadata
+ */
+export const TextObject: IPlugInRegistryEntry<IStoryObject & IPlugIn> = makeObservable({
     name: "TextObject",
     id: "internal.content.text",
+    shortId: "text",
     author: "NGWebS-Core",
     version: "1.0.0",
     class: _TextObject
-}
+}, {
+    name: false,
+    id: false,
+    shortId: false,
+    author: false,
+    version: false,
+    class: false
+});
+
+
+/**
+ * Let's plug ourselves in!
+ */
+// rootStore.storyContentTemplatesRegistry.register([TextObject]);

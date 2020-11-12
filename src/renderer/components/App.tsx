5c3728532bbfdc661d6b10f919e6e1ca915207bb
Philipp Kessling <philipp.kessling@haw-hamburg.de>
2020-11-12 23:22:03 +0100

    add preview

--- src/renderer/components/App.tsx
+++ src/renderer/components/App.tsx
@@ -3,7 +3,7 @@ import { Component, FunctionalComponent, h } from "preact";
 
 import { GalleryItemView } from './GalleryItemView';
 import { Header } from './Header';
-import { Pane, PaneGroup, SideBar } from './Pane';
+import { HiddeableSideBar, Pane, PaneGroup } from './Pane';
 import { StoryComponentGallery } from './StoryComponentGalleryView/StoryComponentGallery';
 import { VerticalPane, VerticalPaneGroup, VerticalSmallPane, VerticalMiniPane } from './VerticalPane/VerticalPane';
 import { Window, WindowContent } from "./Window";
@@ -12,6 +12,7 @@ import { ItemPropertiesView } from './ItemPropertiesView/ItemPropertiesView';
 import { DummyObjectRenderer } from "./DummyObjectRenderer/DummyObjectRenderer";
 import { BreadCrumb } from "./BreadCrumbs/BreadCrumbs";
 import { IStoryObject } from 'storygraph';
+import { Preview } from './Preview/Preview';
 
 interface IAppProps {
     store: RootStore
@@ -34,10 +35,11 @@ export class App extends Component<IAppProps> {
                     title={store.uistate.windowProperties.title}
                     leftToolbar={[
                     <button class="btn btn-default"
-                        onClick={() =>{
-                            store.uistate.toggleSidebar();
+                        onClick={() => {
+                            console.log("Hello");
+                            store.uistate.toggleSidebar("left");
                         }}>
-                        <span class="icon icon-left-dir"></span>
+                        <span class={"icon icon-left-dir"}></span>
                     </button>]}
                 ></Header>
                 <WindowContent>
@@ -54,11 +56,11 @@ interface EditorPaneGroupProperties {
 
 const EditorPaneGroup: FunctionalComponent<EditorPaneGroupProperties> = ({loadedItem, store}) => {
     return <PaneGroup>
-        <SideBar>
+        <HiddeableSideBar uistate={store.uistate}>
             <ItemPropertiesView
                 store={store}>
             </ItemPropertiesView>
-        </SideBar>
+        </HiddeableSideBar>
         {/* <DropzonePane uistate={store.uistate} model={store.model}></DropzonePane> */}
         <Pane>
             <VerticalPaneGroup>
@@ -80,6 +82,15 @@ const EditorPaneGroup: FunctionalComponent<EditorPaneGroupProperties> = ({loaded
                     </StoryComponentGallery>
                 </VerticalSmallPane>
             </VerticalPaneGroup>
+        </Pane>
+        <Pane>
+            <Preview
+                topLevelObjectId={store.uistate.topLevelObjectID}
+                id={"g"}
+                graph={store.storyContentObjectRegistry.getValue(store.uistate.topLevelObjectID)?.childNetwork}
+                registry={store.storyContentObjectRegistry}
+            >
+            </Preview>
         </Pane>
     </PaneGroup>
 };

67c7c9e8e7d55928e4caecd24f1944efc2bb08ea
Philipp Kessling <philipp.kessling@haw-hamburg.de>
2020-11-12 23:21:41 +0100

    make them panes hiddeable

--- src/renderer/components/Pane.tsx
+++ src/renderer/components/Pane.tsx
@@ -1,4 +1,6 @@
-import { FunctionalComponent, h } from 'preact';
+import { Component, FunctionalComponent, h } from 'preact';
+import { IReactionDisposer, reaction } from "mobx";
+import { UIStore } from '../store/UIStore';
 
 export const PaneGroup: FunctionalComponent = ({ children }) => (
     <div class="pane-group">{children}</div>
@@ -21,3 +23,35 @@ export const SmallPane: FunctionalComponent = ({ children }) => (
 export const SideBar: FunctionalComponent = ({ children }) => (
     <div class="pane pane-sm sidebar">{children}</div>
 );
+
+export interface IHideable {
+    // hidden: boolean
+    uistate: UIStore
+    children: h.JSX.Element
+}
+
+export class HiddeableSideBar extends Component<IHideable> {
+
+    reactionDisposer: IReactionDisposer
+
+    constructor(props: IHideable) {
+        super(props);
+
+        this.reactionDisposer = reaction(
+            () => (props.uistate.hideLeftSidebar),
+            () => {
+                this.setState({});
+            }
+        )
+    }
+
+    render({uistate, children}: IHideable): h.JSX.Element {
+        return <div class={"pane pane-sm sidebar" + ((uistate.hideLeftSidebar) ? " hidden" : "")}>
+            {children}
+        </div>
+    }
+
+    componentWillUnmount(): void {
+        this.reactionDisposer();
+    }
+}

bca45178e27de8604ed17bfb10dafa3b3136c558
Philipp Kessling <philipp.kessling@haw-hamburg.de>
2020-11-17 23:39:34 +0100

    move StoryObjectView to its own place

---
+++ src/renderer/components/StoryObjectView/StoryObjectView.tsx
@@ -0,0 +1,68 @@
+import { IReactionDisposer, reaction } from 'mobx';
+import { Component, h } from 'preact';
+import { IStoryObject } from 'storygraph/dist/StoryGraph/IStoryObject';
+import { RootStore } from '../../store/rootStore';
+import { Draggable } from '../Draggable';
+import { MoveSender } from '../Moveable';
+
+
+
+export class StoryObjectView extends Component<StoryObjectViewProperties> {
+
+    reactionDisposer: IReactionDisposer;
+
+    constructor(props: StoryObjectViewProperties) {
+        super(props);
+
+        this.reactionDisposer = reaction(
+            () => ([...props.store.uistate.selectedItems.ids, props.object.name, props.object.content?.resource]),
+            () => {
+                this.setState({});
+            }
+        );
+    }
+
+    render({ store, object, children }: StoryObjectViewProperties): h.JSX.Element {
+
+        return <Draggable id={object.id}>
+            <div class="outer">
+                <div
+                    onClick={(e) => {
+                        e.preventDefault();
+                        const selectedItems = store.uistate.selectedItems;
+                        if (e.shiftKey) {
+                            selectedItems.addToSelectedItems(object.id);
+                        } else {
+                            selectedItems.setSelectedItems([object.id]);
+                        }
+                    }}
+                    onDblClick={(e) => {
+                        e.preventDefault();
+                        if (object.role === "internal.container.container") {
+                            store.uistate.setLoadedItem(object.id);
+                        }
+                    }}
+                    class="story-object-view"
+                >
+                    <MoveSender registry={store.uistate.moveableItems} selectedItems={store.uistate.selectedItems} id={object.id}>
+                        <div class={`area-meta ${(store.uistate.selectedItems.isSelected(object.id)) ? "active" : "inactive"}`}>
+                            {children}
+                        </div>
+                    </MoveSender>
+                    <div class="area-content">
+                        <span>{object.content?.resource}</span>
+                    </div>
+                </div>
+            </div>
+        </Draggable>;
+    }
+
+    componentWillUnmount(): void {
+        this.reactionDisposer();
+    }
+}
+interface StoryObjectViewProperties {
+    store: RootStore;
+    object: IStoryObject;
+    children: string;
+}

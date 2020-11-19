e5efa2e95d42257963d4c3f1f9a402334a3fd4e0
Jannik Golek <jannik@bonedo.de>
2020-11-19 13:42:53 +0100

    [TASK] introduce UI-Themes

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,13 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 sass.compiler = require('node-sass');
+
+gulp.task('sass', function () {
+  return gulp.src('./src/renderer/index.scss')
+    .pipe(concat('./ngwebs.scss'))
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./dist/css/'));
+});
  
 gulp.task('sass', function () {
   return gulp.src('./src/**/*.scss')

fb8583d0f382f40da65608eba478874890c55b38
Philipp Kessling <philipp.kessling@haw-hamburg.de>
2020-11-19 23:37:26 +0100

    refactor PlugIns to sub a common abstract super class

--- TextObject.tsx
+++ TextObject.tsx
@@ -6,31 +6,32 @@ import {IPlugInRegistryEntry, IPlugIn, IMenuTemplate, INGWebSProps } from "../re
 
 import { v4 } from "uuid";
 import { action, computed, makeObservable, observable } from 'mobx';
-import { IStoryObject, IMetaData, IEdge, IRenderingProperties, IStoryModifier, IConnectorPort } from 'storygraph';
+import { IStoryObject, IMetaData, IEdge, IRenderingProperties, IStoryModifier, IConnectorPort, StoryGraph } from 'storygraph';
 import { IContent } from 'storygraph/dist/StoryGraph/IContent';
 import { defaultFields } from './helpers/plugInHelpers';
 import { IRegistry } from 'storygraph/dist/StoryGraph/IRegistry';
+import { AbstractStoryObject } from './helpers/AbstractStoryObject';
 /**
  * Our first little dummy PlugIn
  * 
  * @todo It should actually inherit from StoryObject and not StoryGraph...
  */
 // @observable
-class _TextObject implements IPlugIn, IStoryObject{
-    id = v4();
-    role: string
-    name: string;
-    userDefinedProperties: any;
-    content?: IContent | undefined;
-    metaData: IMetaData;
-    connections: IEdge[];
-    connectors: IConnectorPort[];
-    parent?: string;
-    renderingProperties: IRenderingProperties;
-    modifiers: IStoryModifier[];
-    isContentNode = true;
+class _TextObject extends AbstractStoryObject {
+    
+    public name: string;
+    public role: string;
+    public isContentNode: boolean;
+    public userDefinedProperties: any;
+    public content: IContent;
+    public childNetwork?: StoryGraph | undefined;
+    public connectors: IConnectorPort[];
+    public menuTemplate: IMenuTemplate[];
 
     constructor() {
+
+        super();
+        this.isContentNode = true;
         this.role = "content"
         this.name = "Text" // [this.role, this.id].join("_");
         this.renderingProperties = {
@@ -38,8 +39,6 @@ class _TextObject implements IPlugIn, IStoryObject{
             order: 1,
             collapsable: false
         };
-        this.modifiers = [];
-        this.connections = [];
         this.connectors = [
             {
                 name: "flow-in",
@@ -56,87 +55,36 @@ class _TextObject implements IPlugIn, IStoryObject{
                 type: "reaction",
                 direction: "out"
             }
-        ]
-        this.metaData = {
-            createdAt: new Date(Date.now()),
-            name: "NGWebS user",
-            tags: []
-        };
+        ];
         this.content = {
-            resource: "empty",
+            resource: "Type here...",
             altText: "empty",
             contentType: "text"
         };
         this.userDefinedProperties = {};
+        this.menuTemplate = defaultFields(this);
 
         makeObservable(this, {
             id: false,
-            name: observable,
-            userDefinedProperties: observable,
-            content:    observable,
-            metaData:   observable,
-            connections:   observable,
-            modifiers:  observable,
-            menuTemplate: computed,
-            updateText: action,
-            updateName: action
-            // menuTemplate: computed
-            // inputs:     observable,
-            // outputs:    observable,
-            // parent:     observable,
-            // network:    observable
+            name:                   observable,
+            userDefinedProperties:  observable,
+            content:                observable,
+            updateName:             action,
+            updateText:             action
         });
     }
 
-    // @computed
-    get menuTemplate(): IMenuTemplate[]  {
-        return [
-            {
-                label: "Name",
-                type: "text",
-                valueReference: (name: string) => {this.updateName(name)},
-                value: () => (this.name)
-            },
-            {
-                label: "Content",
-                type: "textarea",
-                valueReference: (text: string) => {this.updateText(text)},
-                value: () => (this.content?.resource as string)
-            }, ...defaultFields(this)
-            // {
-            //     label: "Text",
-            //     type: "textarea",
-            //     valueReference: this.updateName(),
-            //     value: this.getName()
-            // }
-        ]
+    public getEditorComponent(): FunctionComponent<INGWebSProps> {
+        throw new Error('Method not implemented.');
     }
 
-    updateName(newValue: string): void {
+    public updateName(newValue: string): void {
         // eslint-disable-next-line @typescript-eslint/no-this-alias
         this.name = newValue;
     }
 
-    updateText(text: string) {
+    public updateText(text: string) {
         if (this.content) this.content.resource = text;
-    }
-
-    updateConnections(registry: IRegistry, id: string, myport: string, theirport: string, direction: "in" | "out" = "in") {
-        if (this.parent) {
-            const isIncoming = direction === "in";
-
-            const parentNetwork = registry.getValue(this.parent)?.childNetwork;
-            if (parentNetwork) {
-                const newEdge: IEdge = {
-                    id: (isIncoming) ? `edge.${id}.${this.id}` : `edge.${this.id}.${id}`,
-                    from: ((isIncoming) ? `${id}.${theirport}` : `${this.id}.${myport}`),
-                    to: ((isIncoming) ? `${this.id}.${myport}` : `${id}.${theirport}`),
-                    parent: parentNetwork
-                };
-                console.log(newEdge);
-                parentNetwork.connect(registry, [newEdge]);
-            }
-        }
     }
 
     public getComponent() {

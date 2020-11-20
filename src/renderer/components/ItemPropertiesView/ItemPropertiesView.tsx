b4d8a7d32a0a5fb9a8c0238517cc7053100813f8
Philipp Kessling <philipp.kessling@haw-hamburg.de>
2020-11-20 15:33:40 +0100

    move ConnectionTable to it's own file

--- src/renderer/components/ItemPropertiesView/ItemPropertiesView.tsx
+++ src/renderer/components/ItemPropertiesView/ItemPropertiesView.tsx
@@ -2,11 +2,10 @@
 import { remote } from 'electron/renderer';
 const { Menu, MenuItem } = remote;
 
-import { reaction, IReactionDisposer } from 'mobx';
+import { reaction } from 'mobx';
 import { Component, h } from 'preact';
-import { StoryGraph, IConnectorPort, IEdge } from 'storygraph';
 import { RootStore } from '../../store/rootStore';
-import { IMenuTemplate } from '../../utils/PlugInClassRegistry';
+import { ConnectionTableView } from './ConnectionTableView';
 
 export interface IItemPropertiesViewProperties {
     // template: IMenuTemplate[] | undefined
@@ -109,110 +108,4 @@ export class ItemPropertiesView extends Component<IItemPropertiesViewProperties>
     }
 }
 
-export class ConnectionTableView extends Component<IItemView> {
 
-    reactionDisposer: IReactionDisposer
-
-    constructor(props: IItemView) {
-        super(props);
-
-            this.reactionDisposer = reaction(
-                () => props.item.value().connections.length & props.item.value().connectors.length,
-                () => this.setState({})
-            );
-    }
-
-    render({store, item}: IItemView): h.JSX.Element {
-        const myId: string  = item.value().id;
-
-        return <div class="form-group-item">
-            <label>{item.label}</label>
-            <table class="table-striped">
-            {
-                item.value().connectors.map((port: IConnectorPort) => (<tr
-                    onDragOver={(e: DragEvent) => {
-                        e.preventDefault();
-                    }}
-                    onDrop={(e: DragEvent) => {
-                        const id = e.dataTransfer?.getData("text");
-
-                        if (id) {
-                            const direction = port.direction;
-                            const _item = store.storyContentObjectRegistry.getValue(id);
-
-                            if (_item) {
-                                const contextMenu = new Menu();
-                                _item.connectors
-                                .filter(con => (
-                                    con.type === port.type &&
-                                    con.direction !== port.direction
-                                ))
-                                .map((_port: IConnectorPort) => (new MenuItem({
-                                    label: _port.name,
-                                    click: () => {
-                                        console.log("add edge to", id + "." + _port.name);
-                                        if (item.valueReference) {
-                                            item.valueReference(
-                                                store.storyContentObjectRegistry,
-                                                id,
-                                                port.name,
-                                                _port.name,
-                                                direction
-                                            )
-                                        }
-                                    }
-                                })))
-                                .forEach((item: any) => contextMenu.append(item));
-            
-                                contextMenu.popup({
-                                    window: remote.getCurrentWindow(),
-                                    x: e.x,
-                                    y: e.y
-                                });
-                            }                     
-                        }
-                    }}>
-                    <th><strong>{port.name}</strong></th>
-                    {(
-                        item
-                        .value()
-                        .connections as IEdge[])?.filter((edge: IEdge) =>
-                            (port.direction === "in") ?
-                            edge.to.match([myId, port.name].join(".")) :
-                            edge.from.match([myId, port.name].join(".")))
-                            .map((edge: IEdge) => {
-                                    return <tr>{(port.direction === "in") ?
-                                            <td>{store.storyContentObjectRegistry.getValue(StoryGraph.parseNodeId(edge.from)[0])?.name}</td> :
-                                            <td>{store.storyContentObjectRegistry.getValue(StoryGraph.parseNodeId(edge.to)[0])?.name}</td>    
-                                        }
-                                    </tr>
-                    })
-
-                    }
-                </tr>))
-
-            }
-            
-                {/* <tr>
-                    <th><strong>To</strong></th>
-                    <th><strong>From</strong></th>
-                </tr>
-                {(item.value().connections as IEdge[])?.map(edge => {
-                    return <tr>
-                        <td>{store.storyContentObjectRegistry.getValue(StoryGraph.parseNodeId(edge.to)[0])?.name}</td>
-                        <td>{store.storyContentObjectRegistry.getValue(StoryGraph.parseNodeId(edge.from)[0])?.name}</td>
-                    </tr>
-                })} */}
-            </table>
-        </div>
-    }
-
-    componentWillUnmount(): void {
-        this.reactionDisposer();
-    }
-}
-
-export interface IItemView {
-    store: RootStore
-    item: IMenuTemplate
-}

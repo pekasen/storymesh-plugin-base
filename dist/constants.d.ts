9fd3e428fea682783119c9e0a466cac478f8b674
Philipp Kessling <philipp.kessling@haw-hamburg.de>
2020-12-15 17:46:44 +0100

    remove previously tracked files in dist/

--- dist/constants.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="node" />
-export declare const __plattform: NodeJS.Platform;
-export declare const __userHome: string;
-export declare const __appDataPath: string;
-export declare const __prefPath: string;
--- dist/preferences/index.d.ts
+++
@@ -1,7 +0,0 @@
-declare type Themes = "dark" | "light" | "eggplant";
-export declare class Preferences {
-    theme: Themes;
-    author: string;
-    static availableThemes: string[];
-}
-export {};

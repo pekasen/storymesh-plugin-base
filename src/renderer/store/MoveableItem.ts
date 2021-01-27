6853c8dd50d3cd41489200f35ee156270ffce9df
Philipp Kessling <philipp.kessling@haw-hamburg.de>
2021-01-27 17:32:34 +0100

    [add] collapseable story objects

--- src/renderer/store/MoveableItem.ts
+++ src/renderer/store/MoveableItem.ts
@@ -7,18 +7,22 @@ export class MoveableItem { //<T extends IValue>
     id: string;
     public x: number;
     public y: number;
+    public collapsed: boolean;
     
     constructor(id: string, x?: number, y?: number) {
         // this.data = data;
         this.id = id
         this.x = x || 0;
         this.y = y || 0;
+        this.collapsed = false; 
 
         makeObservable(this, {
             // data: observable,
             x: observable,
             y: observable,
-            updatePosition: action
+            collapsed: observable,
+            updatePosition: action,
+            toggleCollapse: action
         });
     }
 
@@ -26,12 +30,17 @@ export class MoveableItem { //<T extends IValue>
         this.x += x;
         this.y += y;
     }
+
+    public toggleCollapse(): void {
+        this.collapsed = !this.collapsed;
+    }
 }
 
 export const MoveableItemSchema = createModelSchema(MoveableItem, {
     id: identifier(),
     x: primitive(),
-    y: primitive()
+    y: primitive(),
+    collapsed: primitive()
 });
 
 setDefaultModelSchema(MoveableItem, MoveableItemSchema);

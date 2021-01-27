cd96adb0b4086bf5b16005312c58eec8c4bbf67d
Philipp Kessling <philipp.kessling@haw-hamburg.de>
2021-01-27 11:57:25 +0100

    Revert "drop"
    This reverts commit e9ece4072beede535db4e4278bcbad5a581923d8.

--- modifiers/GridItem.tsx
+++ modifiers/GridItem.tsx
@@ -1,9 +1,9 @@
 import { makeObservable, observable, runInAction } from "mobx";
-import { createModelSchema, list, map, mapAsArray, object, primitive } from "serializr";
+import { createModelSchema, list, object, primitive } from "serializr";
 import { IMenuTemplate } from "../../renderer/utils/PlugInClassRegistry";
 import { exportClass } from "../helpers/exportClass";
 import { CSSModifier, CSSModifierData, CSSStatement } from "../helpers/CSSModifier";
-import { serializeAsMeshReference } from "babylonjs";
+import { arrayExtensions } from "mobx/dist/internal";
 
 interface IGridItemInlineStatements extends CSSStatement {
     "grid-row": string;
@@ -15,8 +15,8 @@ interface IGridItemModifierData extends CSSModifierData {
 }
 
 export class GridItemInlineStatements implements IGridItemInlineStatements {
-    "grid-row" = "";
-    "grid-column" = "";
+    "grid-row" = "auto";
+    "grid-column" = "auto";
 
     [key: string]: string
 
@@ -28,33 +28,14 @@ export class GridItemInlineStatements implements IGridItemInlineStatements {
     }
 }
 
-type Size = "XS" | "SM" | "MD" | "LG" | "XL";
-
 export class GridItem implements IGridItemModifierData {
     
-    public get classes (): string[] {
-        return ["grid-item"].concat(
-            Array.
-            from(this.classMap).
-            map(e => {
-                return `${e[0].toLowerCase()}-${e[1]}`;
-            })
-        );
-    }
-
+    public classes = ["grid-item"];
     public inline = new GridItemInlineStatements();
-    public classMap = new Map<Size, number>([
-        ["XS", 12],
-        ["SM", 12],
-        ["MD", 6],
-        ["LG", 6],
-        ["XL", 3]
-    ]);
 
     constructor() {
         makeObservable(this, {
             classes: true,
-            classMap: observable,
             inline: observable
         });
     }
@@ -76,6 +57,13 @@ export class CSSGriditemModifier extends CSSModifier {
     public get menuTemplate(): IMenuTemplate[] {
         return [
             ...super.menuTemplate,
+            // {
+            //     label: "Display",
+            //     type: "dropdown",
+            //     value: () => this.data.display,
+            //     valueReference: (value: string) => runInAction(() => this.data.display = (value === "grid") ? value : "inline-grid"),
+            //     options: ["grid", "inline-grid"]
+            // },
             // {
             //     label: "Row Start",
             //     type: "text",
@@ -100,37 +88,23 @@ export class CSSGriditemModifier extends CSSModifier {
             //         // }
             //     }
             // },
+            {
+                label: "Class",
+                value: () => this.data.classes.join(" "),
+                valueReference: (value: string) => runInAction(() => this.data.classes = value.split(" ")),
+                type: "text"
+            }
             // {
-            //     label: "Class",
-            //     value: () => this.data.classes.join(" "),
-            //     valueReference: (value: string) => runInAction(() => this.data.classes = value.split(" ")),
-            //     type: "text"
+            //     label: "Rows",
+            //     type: "text",
+            //     value: () => this.data["grid-template-rows"],
+            //     valueReference: (value: string) => runInAction(() => this.data["grid-template-rows"] = value)
             // },
-            ...["XS", "SM", "MD", "LG", "XL"].map(e => (
-                this.makeSlider(e as Size)
-            )),
         ];
     }
 
     public get getRenderingProperties(): any {
         return super.getRenderingProperties;
-    }
-
-    private makeSlider(key: Size): IMenuTemplate {
-        return {
-            label: key + " Width",
-            value: () => this.data.classMap.get(key),
-            valueReference: (value: string) => {
-                const num = Number(value);
-
-                runInAction(() => this.data.classMap.set(key, num))
-            },
-            type: "hslider",
-            options: {
-                min: 1,
-                max: 12
-            }
-        }
     }
 }
 
@@ -141,8 +115,7 @@ export const GridItemInlineStatementsSchema = createModelSchema(GridItemInlineSt
 
 export const CSSGriditemModifierDataSchema = createModelSchema(GridItem, {
     classes: list(primitive()),
-    data: object(GridItemInlineStatements),
-    classMap: map(primitive())
+    data: object(GridItemInlineStatements)
 });
 
 export const CSSGriditemModifierSchema = createModelSchema(CSSGriditemModifier, {
@@ -151,7 +124,7 @@ export const CSSGriditemModifierSchema = createModelSchema(CSSGriditemModifier, 
 
 export const plugInExport = exportClass(
     CSSGriditemModifier,
-    "Grid Item",
+    "Grid item",
     "internal.modifier.griditem",
     "icon-speaker",
     true

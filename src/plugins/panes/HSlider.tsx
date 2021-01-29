e98de10a7e5376eead90bd6f1234462204136e88
Philipp Kessling <philipp.kessling@haw-hamburg.de>
2021-01-29 16:44:46 +0100

    delegate string formatting to lambda inside options

--- src/plugins/panes/HSlider.tsx
+++ src/plugins/panes/HSlider.tsx
@@ -8,8 +8,6 @@ export class HSliderMenuItem implements IMenuItemRenderer {
     render(item: IMenuTemplate): JSX.Element {
         const pRef = useRef<HTMLParagraphElement>(undefined);
 
-        const formatString = (val: number) => `${val} Column${(val === 1) ? "" : "s"}`
-
         return <div class="form-group-item">
             <label>{item.label}</label>
             <input
@@ -24,12 +22,16 @@ export class HSliderMenuItem implements IMenuItemRenderer {
                     if (item.valueReference && target.value && target.value !== item.value().length) {
                         item.valueReference(target.value);
                         if (pRef && pRef.current && pRef !== null && pRef.current !== null) {
-                            pRef.current.innerHTML = formatString(Number(target.value));
+                            const val = Number(item.value());
+                            const string = (item.options.formatter) ? item.options.formatter(val) : val
+                            pRef.current.innerHTML = string;
                         }
                     }
                 }}
             />
-            <p ref={pRef}>{formatString(item.value())}</p>
+            <p ref={pRef}>{
+                (item.options.formatter) ? item.options.formatter(item.value()) : item.value()
+            }</p>
         </div>
     }
 }

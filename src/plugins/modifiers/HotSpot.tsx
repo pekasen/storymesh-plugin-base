d3c50d20497a13dc40d4f162053297f0236a431c
Philipp Kessling <philipp.kessling@haw-hamburg.de>
2021-01-29 18:35:34 +0100

    add implementation for HotSpot modifier

--- src/plugins/modifiers/HotSpot.tsx
+++ src/plugins/modifiers/HotSpot.tsx
@@ -1,66 +1,127 @@
-import { makeObservable, observable, runInAction } from "mobx";
+import { action, makeObservable, observable, runInAction } from "mobx";
 import { h } from "preact";
+import { useRef } from "preact/hooks";
 import { createModelSchema, list, map, object, primitive } from "serializr";
 import { IMenuTemplate } from "../../renderer/utils/PlugInClassRegistry";
 import { exportClass } from "../helpers/exportClass";
 import { HMTLModifier } from "../helpers/HTMLModifier";
 
+class HotSpot {
+    public x: number;
+    public y: number;
+
+    constructor() {
+        this.x = 0.5;
+        this.y = 0.5;
+
+        makeObservable(this, {
+            x: true,
+            y: true,
+            updateXY: action
+        });
+    }
+
+    public updateXY(x?: number, y?: number) {
+        if (x) {
+            this.x = this._clip(x);
+        }
+        if (y) {
+            this.y = this._clip(y);
+        }
+    }
+
+    public render(svg: SVGSVGElement): h.JSX.Element | null {
+        throw("This method should not be called");
+    }
+
+    protected _clip(x: number, min = 0, max = 1) {
+        return ((x >= min) ? (x < max) ? x : max : min)
+    }
+}
+
+class CircleHotSpot extends HotSpot {
+    public radius: number;
+
+    constructor() {
+        super();
+
+        this.radius = 0.25;
+        makeObservable(this, {
+            radius: true,
+            updateRadius: action
+        });
+    }
+
+    public updateRadius(radius: number) {
+        this.radius = this._clip(radius, 0, Math.PI);
+    }
+
+    public render(svg: SVGSVGElement) {
+        if (svg) {
+            const { width, height } = svg.getBoundingClientRect();
+
+            const relX = width * this.x;
+            const relY = height * this.y;
+            const relR = Math.sqrt(this.x^2 + this.y^2) * this.radius;
+            
+            return <circle cx={relX} cy={relY} r={relR}/>
+        } else return null
+    }
+}
+
+class HotSpotModifierData {
+    [key: string]: HotSpot[]
+
+    constructor() {
+        this.hotspots = [
+            new CircleHotSpot()
+        ];
+
+        makeObservable(this, {
+            hotspots: observable
+        })
+    }
+}
 export class HTMLHotSpotModifier extends HMTLModifier {
     public name = "Hot-o-Spot-o"
     public role = "internal.modifier.hotspot";
+    public data = new HotSpotModifierData();
 
     constructor() {
         super();
 
         makeObservable(this, {
-            data: observable
+            data: observable,
+            addHotSpot: action,
+            removeHotSpot: action
         })
     }
 
     public modify(element: h.JSX.Element): h.JSX.Element {
-        element.props.usemap = `#${this.id}`;
+        // element.props.usemap = `#${this.id}`;
+        const svg = useRef<SVGSVGElement>();
+        
+        return <div id={this.id}>
+        <svg ref={svg}>
+            {this.data.hotspots.map(e => e.render(svg.current))}
+        </svg>
+        {element}
+    </div>;
+    }
 
+    public addHotSpot(hotspot: HotSpot): void {
+        this.data.hotspots.push(hotspot);
+    }
 
-
-        return <div >
-            {element}
-
-        </div>;
+    public removeHotSpot(hotspot: HotSpot): void {
+        this.data.hotspots.splice(
+            this.data.hotspots.indexOf(hotspot), 1
+        )
     }
 
     public get menuTemplate(): IMenuTemplate[] {
         return [
             ...super.menuTemplate,
-            // {
-            //     label: "Row Start",
-            //     type: "text",
-            //     value: () => this.data.inline["grid-row"],
-            //     valueReference: (value: string) => {
-            //         // validate user input
-            //         runInAction(() => this.data.inline["grid-row"] = value)
-            //         // const test = /\d+px/gm;
-            //         // if (test.test(value)) {
-            //         // }
-            //     }
-            // },
-            // {
-            //     label: "Column Start",
-            //     type: "text",
-            //     value: () => this.data.inline["grid-column"],
-            //     valueReference: (value: string) => {
-            //         // validate user input
-            //         runInAction(() => this.data.inline["grid-column"] = value)
-            //         // const test = /\d+px/gm;
-            //         // if (test.test(value)) {
-            //         // }
-            //     }
-            // },
-            // {
-            //     label: "Class",
-            //     value: () => this.data.classes.join(" "),
-            //     valueReference: (value: string) => runInAction(() => this.data.classes = value.split(" ")),
-            //     type: "text"
-            // },
         ];
     }
 

9bb2858cf1d9255faa0e19a6d6447a692487f1e3
Philipp Kessling <philipp.kessling@haw-hamburg.de>
2021-02-16 13:36:33 +0100

    poke around with connectionTable bug

--- src/plugins/panes/ConnectionTable.tsx
+++ src/plugins/panes/ConnectionTable.tsx
@@ -21,7 +21,11 @@ export class ConnectionTableMenuItem implements IMenuItemRenderer {
             const reactionDisposer = reaction(
                 () => {
                     const obj = item.value();
-                    [obj.connections.length, obj.connectors.size, ...obj.connections]
+                    [
+                        obj.connections.length,
+                        obj.connectors.size,
+                        // ...obj.connections
+                    ]
                 },
                 () => setState({})
             );

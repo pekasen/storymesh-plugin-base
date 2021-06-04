4981bcf954b08886033e22f2d3c564a7391ef8cd
Philipp Kessling <philipp.kessling@haw-hamburg.de>
2021-06-04 09:02:38 +0200

    remove helpers, since, we require them from storygraph

--- src/helpers/IMenuItemRenderer.tsx
+++
@@ -1,5 +0,0 @@
-import { MenuTemplate } from "preact-sidebar";
-// TODO: is this depreacated and can be removed?
-export interface IMenuItemRenderer {
-    render(item: MenuTemplate): preact.JSX.Element;
-}
